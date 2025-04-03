fcb47f37e93447074eed08f25b6e1ce389c25a9a
Adam Chou <adamchoubusiness@gmail.com>
2025-04-03 00:21:30 -0400

    more changes to styling, fixing landing

--- src/navbar/page.tsx
+++ src/navbar/page.tsx
@@ -1,9 +1,33 @@
-import styles from "styles.module.css";
+import styles from "./styles.module.css";
 
 export default function NavBar() {
     return(
       <div>
-  
+        {/* Rainbow Bar */}
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Nav Bar */}
+        <nav id={`${styles.nav}`}>
+            <div id={`${styles.left}`} className={`${styles.sidecontainer}`}>
+                <h1>_AC</h1>
+            </div>
+            <div id={`${styles.middle}`} className={`${styles.middlecontainer}`}></div>
+            <div id={`${styles.right}`} className={`${styles.sidecontainer}`}>
+                <ul id={`${styles.navbox}`} className={`${styles.navcontainer}`}>
+                    <h4>| WORK</h4>
+                    <h4>| MEDIA</h4>
+                    <h4>| ABOUT</h4>
+                    <br></br>
+                    <h4>| CONTACT</h4>
+                </ul>
+            </div>
+        </nav>
       </div>
     )
   }
--- src/pages/landing/page.tsx
+++ src/pages/landing/page.tsx
@@ -3,21 +3,23 @@ import pagestyles from "../pagestyles.module.css"
 
 export default function Landing() {
     return(
-      <div className={pagestyles.pageContainer}>
+      <div className={`${pagestyles.pageContainer} ${pagestyles.flexbox}`}>
 
-        <div className={pagestyles.backgroundContainer}>
-          <div className={pagestyles.backgroundImage}>
-
-          </div>
-        </div>
-
-        <div className={pagestyles.contentContainer}>
-          <div>
+        <div className={`${pagestyles.contentContainer} ${pagestyles.flexbox}`}>
+          <div className={pagestyles.flexbox}>
             <h1>Hi, I'm Adam,</h1>
             <h2>a Sales Analyst currently based in NYC.</h2>
           
           </div>
         </div>
+        
+        <div className={pagestyles.backgroundContainer}>
+          <div className={styles.backgroundImage}>
+
+          </div>
+        </div>
+
+        
       
       </div>
     )
